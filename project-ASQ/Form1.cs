ebccafc64ab253b252b7e77ecd4c2b773d7bb11f
houssemaloui92 <2695891@collegelacite.ca>
2021-04-22 13:10:46 -0400

    update

--- project-ASQ/Form1.cs
+++ project-ASQ/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,40 @@ namespace project_ASQ
         public Form1()
         {
             InitializeComponent();
+        }
+        public static string dirParameter = AppDomain.CurrentDomain.BaseDirectory + @"\file.txt";
+        public void SaveEvent(string ch1)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Do you want to save file?", "Confirmer", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    if (Resfinal != null)
+                    {
+                        saveFile(ch1);
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.ToString());
+                }
+            }
+        }
+
+        private void saveFile(string ch1)
+        { // Save File to .txt
+            FileStream fParameter = new FileStream(dirParameter, FileMode.Create, FileAccess.Write);
+            StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+            m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+            m_WriterParameter.Write(ch1);
+            m_WriterParameter.Flush();
+            m_WriterParameter.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -41,18 +76,19 @@ namespace project_ASQ
 
         private void enregistrer_Click(object sender, EventArgs e)
         {
-            List<string> data = new List<string>();
+            string res = "";
             foreach (string s in Resultat3.Items)
             {
                 if (id2.Text.Equals(input.Text))
                 {
 
                     Resfinal.Items.Add(s);
-                    data.Add(s);
+                    res += s;
                 }
             }
 
-            StreamWriterTwo.SaveDataAsync(data);
+            SaveEvent(res);
+           
         }
     }
 }
